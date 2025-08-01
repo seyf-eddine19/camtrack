a0fd0ef1f6be580fec12cba6504e6d899e12152f
Seyf Eddine <84080839+seyf-eddine19@users.noreply.github.com>
2025-08-01 23:38:05 +0100

    Update camtrack/settings.py

--- camtrack/settings.py
+++ camtrack/settings.py
@@ -115,6 +115,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
